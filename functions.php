f9a9ede95b5460e075799f2813b6d471dc575c95
rodijanssen <74348802+rodijanssen@users.noreply.github.com>
2022-10-20 13:50:18 +0200

    update
    update functions

--- functions.php
+++ functions.php
@@ -1,5 +1,16 @@
 <?php
 
+global $dealer;
+global $player;
+global $deck;
+global $nextCard;
+global $playertotal;
+$dealer = 0;
+$player = 0;
+
+function createdeck()
+{
+    global $deck;
     $suits = array('C-', 'D-', 'H-', 'S-');
     $cards = array('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K');
 
@@ -12,105 +23,79 @@
             $deck[] = $suit . $card;
         }
     }
-    
+}
+ 
+function shuffledeck()
+{
+    global $deck;
     shuffle($deck);
     shuffle($deck);
+}
 
+// function nextCard($deck)
+// {
+//     $nextCard = $deck[0];
+//     array_splice($deck, 0, 1);
+//     return [$nextCard, $deck];
+// }
 
-    // foreach($deck as $deckcard)
-    // {
-    //     echo $deckcard . "<br />";
-    // }
-
-    // echo "<br /> öof <br />";
-
-
-    $firstCard = $deck[0];
+function drawCardplayer()
+{
+    global $nextCard;
+    global $deck;
+    $nextCard = $deck[0];
     array_splice($deck, 0, 1);
-    echo $firstCard . "<br /><br />";
+    echo $nextCard . "<br />";
+    cardvalue();
+}
 
-    $kaartinfo = explode("-", $firstCard);
-    $typeCard = $kaartinfo[0];
+function cardvalue()
+{
+    global $nextCard;
+    global $playertotal;
+    $kaartinfo = explode("-", $nextCard);
+    // $typeCard = $kaartinfo[0];
     $valueCard = $kaartinfo[1];
-    
-    $totaal = [];
-
-
-    switch($typeCard)
+    if ($valueCard == "A")
     {
-        case 'C':
-            echo "dit is een klaveren";
-            break;
-        case 'D':
-            echo "dit is een ruiten";
-            break;
-        case 'H':
-            echo "dit is een harten";
-            break;
-        case 'S':
-            echo "dit is een schoppen";
-            break;
+        $playertotal += 1;
     }
-
-    ?>
-        <br />
-    <?php
-
-    switch($valueCard)
+    else if ($valueCard == 'J' or $valueCard == 'Q' or $valueCard == 'K')
     {
-        case 'A':
-            echo "Dit heeft een waarde van 1 of 11";
-            array_push($totaal, 11);
-            break;
-        case '2':
-            echo "Dit heeft een waarde van 2";
-            array_push($totaal, 2); 
-            break;
-        case '3':
-            echo "Dit heeft een waarde van 3";
-            array_push($totaal, 3); 
-            break;
-        case '4':
-            echo "Dit heeft een waarde van 4";
-            array_push($totaal, 4);
-            break;
-        case '5':
-            echo "Dit heeft een waarde van 5";
-            array_push($totaal, 5);
-            break;
-        case '6':
-            echo "Dit heeft een waarde van 6";
-            array_push($totaal, 6);
-            break;
-        case '7':
-            echo "Dit heeft een waarde van 7";
-            array_push($totaal, 7);
-            break;
-        case '8':
-            echo "Dit heeft een waarde van 8";
-            array_push($totaal, 8); 
-            break;
-        case '9':
-            echo "Dit heeft een waarde van 9";
-            array_push($totaal, 9);
-            break;
-        case '10';
-        case 'J';
-        case 'Q';
-        case 'K';
-            echo "Dit heeft een waarde van 10";
-            array_push($totaal, 10);
-            break;
+        $playertotal += 10;
     }
+    else
+    {
+        $playertotal += intval($valueCard);
+    }
+    return $playertotal . "<br />";
 
-    ?>
-        <br />
-    <?php
+}
+function echocardvalue()
+{
+    global $playertotal;
+    echo $playertotal;
+}
 
+createdeck();
+shuffledeck();
+drawCardplayer();
+drawCardplayer();
+drawCardplayer();
+echocardvalue();
 
-    print($totaal[0]);
-    
-
-    
-
-?>
+//     switch($typeCard)
+//     {
+//         case 'C':
+//             echo "dit is een klaveren";
+//             break;
+//         case 'D':
+//             echo "dit is een ruiten";
+//             break;
+//         case 'H':
+//             echo "dit is een harten";
+//             break;
+//         case 'S':
+//             echo "dit is een schoppen";
+//             break;
+//     }
